ded15b8a2f2bc02779709414e3ac5b9bf106960f
SAEMMM <yangpa2014@gmail.com>
2023-04-17 11:23:56 +0900

    todo에서 done으로 이동 기능(만) 구현..

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function TodoIng(props) {
+function Todo(props) {
   return (
-    <div className='body-cards'>
-      <div>
-        <h3>{props.todo.title}</h3>
-        <p>{props.todo.content}</p>
-        <div className='btns'>
-          <button onClick={() => props.delBtnHandler(props.todo.id)}
-            className='delBtn'>포기하기</button>
-          <button onClick={() => props.doneBtnHandler(props.todo.id)}
-            className='doneBtn'>내가해냄</button>
+      <div className='body-cards'>
+        <div>
+          <h3>{props.todo.title}</h3>
+          <p>{props.todo.content}</p>
+          <div className='btns'>
+            <button onClick={() => props.delBtnHandler(props.todo.id)}
+              className='delBtn'>포기하기</button>
+            <button onClick={() => props.doneBtnHandler(props.todo.id)}
+              className='doneBtn'>내가해냄</button>
+          </div>
         </div>
       </div>
-    </div>
   )
 }
 
-
 function App() {
+  // 열심히 하는 중 list
   const [todo, setTodo] = useState([
     { id: 1, title: '리액트 장인되기1', content: 'R반의 리액트 장인이 되는 것1' },
     { id: 2, title: '리액트 장인되기2', content: 'R반의 리액트 장인이 되는 것2' },
   ])
 
+  // 완료 list
+  const [doneTodo, setDoneTodo] = useState([])
+
+  // input
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
 
+  // input 내보내주기
   const titleChangeHandler = (event) => {
     setTitle(event.target.value)
   }
@@ -35,6 +40,7 @@ function App() {
     setContent(event.target.value)
   }
 
+  // todo 추가하기
   const clickAddButtonHandler = () => {
     const newTodo = {
       id: todo.length + 1,
@@ -47,11 +53,27 @@ function App() {
     setContent('')
   }
 
+  // 포기하기
   const delBtnHandler = (id) => {
     const newTodoList = todo.filter((todo) => todo.id !== id)
     setTodo(newTodoList)
   }
-  const doneBtnHandler = (id) => {
+
+  // 내가해냄(todo에서 done으로 이동)
+  const doneBtnHandler = (done) => {
+    const newDoneTodo = {
+      id: done.id,
+      title: done.title,
+      content: done.content
+    }
+
+    setDoneTodo([...doneTodo, newDoneTodo])
+    setTodo(todo.filter((v) => v.id !== done.id))
+  }
+
+  // 다시하기(done에서 todo로 이동)
+  const reDoBtnHandler = () => {
+
   }
 
   return (
@@ -71,14 +93,29 @@ function App() {
       <div className='body-style'>
         <h2>열심히 하는 중.. 🔥</h2>
         {todo.map((todo) => {
-          return <TodoIng
-            todo={todo}
-            key={todo.id}
-            delBtnHandler={delBtnHandler}
-            doneBtnHandler={doneBtnHandler} />
+          return (
+            <Todo
+              todo={todo}
+              title={todo.title}
+              content={todo.content}
+              key={todo.id}
+              delBtnHandler={delBtnHandler}
+              doneBtnHandler={doneBtnHandler} />)
         })}
+
         <div className='float-none'></div>
+
         <h2>해내고 말았다!! 🎉</h2>
+        {doneTodo.map((done) => {
+          return (
+            <Todo
+              todo={done}
+              title={done.title}
+              content={done.content}
+              key={done.id}
+              delBtnHandler={delBtnHandler}
+              reDoBtnHandler={reDoBtnHandler} />)
+        })}
       </div>
     </div>
   );
